d122956385560262c35ff86a79fbe6a067834170
richfan <richffan@outlook.com>
2025-10-17 09:30:07 +0800

    20251017093006

--- d.sh
+++ d.sh
@@ -2,7 +2,7 @@
 
 echo -e "-------------------- 开始部署 --------------------"
 
-git submodule update --remote --merge source/_posts
+# git submodule update --remote --merge source/_posts
 # git submodule update --remote --merge themes/stellar
 
 echo -e "------------------- 子模块更新完成 -------------------"
